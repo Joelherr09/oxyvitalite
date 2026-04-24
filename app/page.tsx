1e878be1a79e10412929a48ce28b3e7282dd927e
Joel Herrera Leiva <joelherreraleiva@gmail.com>
2026-04-23 23:25:23 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -10,24 +10,18 @@ import LocationMap from "./components/LocationMap";
 export default function Home() {
   return (
     <main className="bg-white">
-      {/* Hero principal */}
       <Hero />
 
-      {/* Sección de beneficios (grid) */}
       <Benefits />
 
-      {/* Para quién / servicios */}
       <Services />
 
-      {/* Banner promocional de apertura */}
       <PromoBanner />
 
-      {/* Galería de imágenes desde Instagram (9:16)    */}
       <ImageGallery />
 
       <LocationMap />
 
-      {/* Formulario de contacto / reserva */}
       <ContactForm />
     </main>
   );
